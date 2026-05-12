50973d6667b5fc4953059fac4de22c4448d50061
Najia <anajia231@gmail.com>
2026-05-12 19:54:38 +0430

    Fetch cart items from API and pass cart data to relevant components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { Routes, Route} from 'react-router';
-import { HomePage } from './pages/HomePage';
-import { Checkout } from './pages/CheckoutPage';
+import { HomePage } from './pages/home/HomePage';
+import { Checkout } from './pages/CheckoutPage/CheckoutPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  
+  useEffect(() => {
+    const cartData = async () => {
+      const response = await axios.get('/api/cart-items?expand=product');
+      setCart(response.data);
+    }
+
+    cartData();
+  }, [])
 
   return (
     <>
       <Routes>
-        <Route index element={<HomePage/>} />
-        <Route path='checkout' element={<Checkout/>} />
+        <Route index element={<HomePage cart={cart}/>} />
+        <Route path='checkout' element={<Checkout cart={cart}/>} />
+        <Route path='orders' element={<OrdersPage cart={cart}/>} />
+        <Route path='tracking' element={<TrackingPage/>} />
       </Routes>
     </>
   )
